e523d230be91baa3415711cf4240460912845e20
ulg1996 <108278088+ulg1996@users.noreply.github.com>
2022-07-10 14:49:16 +0200

    Added days of forecast to a loop in JS

--- src/index.js
+++ src/index.js
@@ -32,6 +32,38 @@ function formatDay(timestamp) {
   return days[day];
 }
 
+//forecast
+
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
+
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+      <div class="col-2">
+        <div class="weather-forecast-date">${day}</div>
+        <img
+          src="http://openweathermap.org/img/wn/50d@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> 18° </span> |
+          <span class="weather-forecast-temperature-min"> 12° </span>
+        </div>
+      </div>
+  `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+  console.log(forecastHTML);
+}
+
 // show current weather conditions in searched for city
 
 function displayTemperature(response) {
@@ -116,3 +148,4 @@ let currentLocationButton = document.querySelector("#current-location-button");
 currentLocationButton.addEventListener("click", getCurrentLocation);
 
 search("Düsseldorf");
+displayForecast();
